11abbf3e319da1a52bf9400c3166226bd0176734
Oliver Kraitschy <okraits@arcor.de>
2013-10-27 19:14:40 +0100

    Improved index view and prepared it for displaying an album.

--- gallery/views.py
+++ gallery/views.py
@@ -8,39 +8,42 @@ from django.conf import settings
 from os import listdir, mkdir, system
 
 def index(request, folder_id=None):
-    folder_list = Folder.objects.filter(parentfolder=folder_id)
-    
+    context = {'title': settings.SG_TITLE}
     if folder_id is not None:
-        # check only folders which have this folder as parentfolder
-        context = {'title': settings.SG_TITLE,
-                   'folder_list': folder_list,
-                   'folder_id': folder_id}
+        selectedFolder = Folder.objects.get(id__exact=folder_id)
+    if folder_id is not None and selectedFolder.isAlbum:
+        pass
     else:
-        # check only folders which have no parentfolder
-        context = {'title': settings.SG_TITLE,
-                   'folder_list': folder_list}
-    for folder in folder_list:
-        if folder.isAlbum:
-            picfolder_path = settings.SG_UPLOAD_PATH + folder.folderpath + "/"
-            thumbfolder_path = settings.SG_THUMBS_PATH + folder.folderpath + "/"
-            try:
-                piclist = listdir(picfolder_path)
-            except (IOError, OSError):
-                return HttpResponseNotFound("Path %s not found." % (picfolder_path))
-            try:
-                thumblist = listdir(thumbfolder_path)
-            except (IOError, OSError):
-                # no thumbs found for this folder - we have to generate them
+        folder_list = Folder.objects.filter(parentfolder=folder_id)
+        context['folder_list'] = folder_list
+        if folder_id is not None:
+            context['folder_id'] = folder_id
+        for folder in folder_list:
+            if folder.isAlbum:
+                picfolder_path = settings.SG_UPLOAD_PATH + folder.folderpath + "/"
+                thumbfolder_path = settings.SG_THUMBS_PATH + folder.folderpath + "/"
                 try:
-                    mkdir(thumbfolder_path)
+                    piclist = listdir(picfolder_path)
+                    context['piclist'] = piclist
                 except (IOError, OSError):
-                    return HttpResponseNotFound("Error while creating thumbnail folder.")
-                for pic in piclist:
-                    picpath = picfolder_path + pic
-                    thumbpath = thumbfolder_path + "thumb_" + pic
-                    retval = system("convert \"%s\" -resize %sx%s -quality %s \"%s\"" % (picpath, folder.thumbSizeX,
-                                                                       folder.thumbSizeY,
-                                                                       folder.thumbQuality, thumbpath))
-                    if retval != 0:
-                        return HttpResponseNotFound("Error while generating thumbnails.")
+                    return HttpResponseNotFound("Path %s not found." % (picfolder_path))
+                try:
+                    thumblist = listdir(thumbfolder_path)
+                    context['thumblist'] = thumblist
+                except (IOError, OSError):
+                    # no thumbs found for this folder - we have to generate them
+                    try:
+                        mkdir(thumbfolder_path)
+                    except (IOError, OSError):
+                        return HttpResponseNotFound("Error while creating thumbnail folder.")
+                    for pic in piclist:
+                        picpath = picfolder_path + pic
+                        thumbpath = thumbfolder_path + "thumb_" + pic
+                        retval = system("convert \"%s\" -resize %sx%s -quality %s \"%s\"" % (picpath, folder.thumbSizeX,
+                                                                           folder.thumbSizeY,
+                                                                           folder.thumbQuality, thumbpath))
+                        if retval != 0:
+                            return HttpResponseNotFound("Error while generating thumbnails.")
+                    thumblist = listdir(thumbfolder_path)
+                    context['thumblist'] = thumblist
     return render(request, 'gallery/index.html', context)
