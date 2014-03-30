e9575b1854d135de79f3d60aa6f4af83eef9ccb3
Oliver Kraitschy <okraits@arcor.de>
2014-03-30 20:46:45 +0200

    Updated fcgi.sh.

--- fcgi.sh
+++ fcgi.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-PROJDIR="/home/sc-pfettrach/galerie"
-PIDFILE="$PROJDIR/galerie.pid"
-SOCKET="$PROJDIR/galerie.sock"
+PROJDIR="/home/sc-pfettrach/slimgallery"
+PIDFILE="$PROJDIR/slimgallery.pid"
+SOCKET="$PROJDIR/slimgallery.sock"
 
 cd $PROJDIR
 if [ -f $PIDFILE ]; then
@@ -15,4 +15,4 @@ fi
 #  ./manage.py runfcgi socket=$SOCKET pidfile=$PIDFILE
 exec /usr/bin/env - \
   PYTHONPATH="../python:.." \
-  ./manage.py runfcgi method=threaded host=127.0.0.1 port=3033
+  ./manage.py runfcgi method=threaded host=127.0.0.1 port=3034
